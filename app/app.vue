10e9d8379338c6350e7842a397c626132488202a
Katy248 <petrovanton247@gmail.com>
2025-10-13 20:55:51 +0300

    Добавил ссылки в `footer` согласно "ТЗ", переименовал файл

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="min-h-screen flex flex-col font-sans bg-gray-900">
-    <main class="flex-grow">
-    <Navbar />
-      
-      <NuxtPage />
-    <Footer />
-
-    </main>
-  </div>
+    <div class="min-h-screen flex flex-col font-sans bg-gray-900">
+        <main class="flex-grow">
+            <Navbar />
+            <NuxtPage />
+            <TheFooter />
+        </main>
+    </div>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar.vue'
-import Footer from '~/components/Footer.vue'
+import Navbar from "~/components/Navbar.vue";
+import TheFooter from "~/components/TheFooter.vue";
 </script>
--- app/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer class="bg-gray-900 text-gray-300 py-6 text-center">
-    <p>© {{ new Date().getFullYear() }} Արմ Տանս. Все права защищены.</p>
-  </footer>
-</template>
